6ea00e4414ee3e10dca7d064a9ab5811834a3fd1
Michael Morrissey <mikehmorrissey@gmail.com>
2016-05-05 16:05:27 -0400

    start twitter stream

--- bot-api/bot/index.js
+++ bot-api/bot/index.js
@@ -1,9 +1,41 @@
-import TelegramBot from 'node-telegram-bot-api'
-const telegramToken = process.env.TELEGRAM_TOKEN
-const bot = new TelegramBot(telegramToken, { polling: true })
-console.log('hello')
+import Twit from 'twit'
 
-bot.onText(/hello/, (message, match) => {
-  const fromId = message.from.id
-  bot.sendMessage(fromId, 'hello')
+const bot = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
+
+// connect to stream mentioning bot
+const stream = Bot.stream('statuses/filter', {
+  track: [process.env.BOT_USERNAME]
+})
+
+// main reply function
+const replyToTweet = (id, text) => {
+  bot.post('statuses/update', {
+    status: text,
+    in_reply_to_status_id: id,
+  })
+}
+
+// debugging
+if (process.env.OWNER_USERNAME) {
+  const owner = process.env.OWNER_USERNAME
+
+  stream.on('error', (error) => {
+    const errorMessage = `${owner} I need some help! getting the error ${error}`
+    replyToTweet(null, errorMessage)
+  })
+
+  stream.on('limit', (error) => {
+    replyToTweet(null, `${owner} I'm hitting a limit! ${error}!`)
+  })
+}
+
+stream.on('tweet', (tweet) => {
+  const replyTo = `@${tweet.user.screenName}`
+  replyToTweet(tweet.id_str, `${replyTo} hello`)
+})
+
